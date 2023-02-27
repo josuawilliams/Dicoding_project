aa7de34a0c5e6bedf3913ef40bd8c685e958ab60
josuawilliams <josuawilliams17@gmail.com>
2023-02-27 16:26:36 +0700

    build API

---
+++ src/notes.js
@@ -0,0 +1,12 @@
+let notes = [
+  {
+    title: 'sawadikap',
+    tags: 'Facebook',
+    body: 'Ini Tags Lhoo',
+    id: 'kxAyYR8xxTaBowUm',
+    createdAt: '2023-02-27T08:23:43.497Z',
+    updatedAt: '2023-02-27T08:23:43.497Z'
+  }
+]
+
+module.exports = notes
